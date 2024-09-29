9a51d8dba25c6b70ceb960f893f4ff25c8ac8aef
Elmira1703 <milya.nuriyeva@gmail.com>
2024-09-29 23:31:24 +0600

    Add files via upload

--- request.py
+++ request.py
@@ -4,16 +4,14 @@ import data
 
 def post_new_order(body_order):
     return requests.post(configuration.URL_SERVICE + configuration.NEW_ORDER_PATH, json=body_order)
-response = post_new_order(data.body_order)
-print("Создание заказа:", response.status_code)
-print(response.json())
-
-track_number = response.json().get('track')
-print("Номер трека:", track_number)
 
 def order_track(track_number):
     return requests.get(configuration.URL_SERVICE + configuration.ORDER_TRACK_PATH + str(track_number))
 
+response = post_new_order(data.body_order)
+
+track_number = response.json().get('track')
 response_track = order_track(track_number)
-print("Получение заказа по треку:", response_track.status_code)
+
+
 
